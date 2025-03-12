c00c2be1eab7b76d2096a08c6b71560df249dea3
Shivam Ishwar <102852506+shivamishwar07@users.noreply.github.com>
2025-03-13 01:14:23 +0530

    Responsiveness fixes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  imports: [FormsModule,CommonModule]
 })
 export class HeaderComponent {
   languages = [
     { code: 'en', label: 'English' },
-    { code: 'es', label: 'Español' },
-    { code: 'fr', label: 'Français' }
+    { code: 'hi', label: 'हिन्दी' },
+    { code: 'bn', label: 'বাংলা' },
+    { code: 'ta', label: 'தமிழ்' },
+    { code: 'te', label: 'తెలుగు' },
+    { code: 'mr', label: 'मराठी' },
+    { code: 'gu', label: 'ગુજરાતી' },
+    { code: 'pa', label: 'ਪੰਜਾਬੀ' },
+    { code: 'ml', label: 'മലയാളം' },
+    { code: 'kn', label: 'ಕನ್ನಡ' },
+    { code: 'or', label: 'ଓଡ଼ିଆ' },
+    { code: 'as', label: 'অসমীয়া' },
+    { code: 'ur', label: 'اردو' }
   ];
+  
   selectedLanguage = 'en';
   isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  changeLanguage(event: any) {
+    this.selectedLanguage = event.target.value;
+    console.log('Language changed to:', this.selectedLanguage);
   }
 }
