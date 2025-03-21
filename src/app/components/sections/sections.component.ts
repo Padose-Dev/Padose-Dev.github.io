c18a5abfd6d9629009e368ef0b3c32554f2722a7
Shivam Ishwar <102852506+shivamishwar07@users.noreply.github.com>
2025-03-21 19:39:53 +0530

    issues fixes

--- src/app/components/sections/sections.component.ts
+++ src/app/components/sections/sections.component.ts
@@ -15,53 +15,53 @@ export class SectionsComponent {
     Buyer: [
       {
         image: './imgs/buyer/buyerModuleImage1.svg',
-        description: 'Sell more with increased product offerings and personal interaction.'
+        description: 'Game changing inconvenience to convenience'
       },
       {
         image: './imgs/buyer/buyerModuleImage2.svg',
-        description: 'Don\'t miss the opportunity to boost your livelihood.'
+        description: 'Insanely Fresh: Fresh vegetables and fruits to home in no time.'
       },
       {
         image: './imgs/buyer/buyerModuleImage3.svg',
-        description: 'Sell more with increased product offerings and personal interaction.'
+        description: ' Our smart app is easy to track your profits '
       },
       {
         image: './imgs/buyer/buyerModuleImage5.svg',
-        description: 'Don\'t miss the opportunity to boost your livelihood.'
+        description: 'No more waiting for days for emergency or urgent orders.'
       },
       {
         image: './imgs/buyer/buyerModuleImage6.svg',
-        description: 'Sell more with increased product offerings and personal interaction.'
+        description: 'Easy ordering!'
       },
       {
         image: './imgs/buyer/buyerModuleImage4.svg',
-        description: 'Don\'t miss the opportunity to boost your livelihood.'
+        description: 'We priorotize food and products that is locally-grown, seasonal and sold from local people.'
       },
       {
         image: './imgs/buyer/deviceImage1.svg',
-        description: 'Sell more with increased product offerings and personal interaction.'
+        description: ''
       },
       {
         image: './imgs/buyer/deviceImage1.svg',
-        description: 'Don\'t miss the opportunity to boost your livelihood.'
+        description: ''
       }
     ],
     Seller: [
       {
         image: './imgs/seller/sellerModuleImage1.svg',
-        description: 'Sell more with increased product offerings and personal interaction.'
+        description: 'Sell more with  increased product offerings and more personal interaction.'
       },
       {
         image: './imgs/seller/sellerModuleImage2.svg',
-        description: 'Don\'t miss the opportunity to boost your livelihood.'
+        description: ' Dont miss the opportunity to boost your livelihood and be a happier, more satisfied seller.'
       },
       {
         image: './imgs/seller/sellerModuleImage3.svg',
-        description: 'Sell more with increased product offerings and personal interaction.'
+        description: 'Our smart app is easy to track your profits.'
       },
       {
         image: './imgs/seller/sellerModuleImage4.svg',
-        description: 'Don\'t miss the opportunity to boost your livelihood.'
+        description: 'We’re here to help you stock up on your produce based on your sales.'
       },
       {
         image: './imgs/buyer/deviceImage1.svg',
